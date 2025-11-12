5c846e1f4bfa8411d1a8e48ad392dc49d3cae204
kacperunia <kacpermarzol@gmail.com>
2025-11-12 22:02:16 +0100

    test

--- swift/plugin/orm.py
+++ swift/plugin/orm.py
@@ -510,9 +510,10 @@ class DenoisingReward(ORM):
             noisy_latents = self.scheduler.add_noise(clean_latents, noise, t)
 
             predicted_noise = self.unet(noisy_latents, t, encoder_hidden_states=encoder_hidden_states).sample
-            # loss = F.mse_loss(predicted_noise, noise, reduction="mean")
-            loss = F.l1_loss(predicted_noise, noise, reduction="mean")
-            return -loss 
+            loss_mse = F.mse_loss(predicted_noise, noise, reduction="mean")
+            loss_l1 = F.l1_loss(predicted_noise, noise, reduction="mean")
+            print(f"[DenoisingReward], MSE Loss: {loss_mse.item():.6f}, L1 Loss: {loss_l1.item():.6f}")
+            return - (8 * loss_mse.item() + 2 * loss_l1.item())
         except Exception as e:
             print(f"[DenoisingReward] Error in _get_reward_score for prompt '{ap[:50]}...': {e}")
             return -10000
