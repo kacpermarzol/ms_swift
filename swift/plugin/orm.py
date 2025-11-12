9b78f2c462c483c3ae8d84a93536da7edaba5f10
kacperunia <kacpermarzol@gmail.com>
2025-11-12 21:12:15 +0100

    fix

--- swift/plugin/orm.py
+++ swift/plugin/orm.py
@@ -474,7 +474,7 @@ class DenoisingReward(ORM):
             self.vae.requires_grad_(False)
             self.text_encoder.requires_grad_(False)
             self.unet.requires_grad_(False)
-            self.tokenizer.eval()
+            # self.tokenizer.eval()
             self.tokenizer.requires_grad_(False)
 
             self.seed = 1234
